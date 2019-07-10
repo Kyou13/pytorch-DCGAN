84c74012754e66813c75696dfad656898a086ebf
Kyou13 <kyou1013@gmail.com>
2019-07-11 00:58:09 +0900

    [add] training image

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ import matplotlib.pyplot as plt
 
 params = {
     'seed': 123456789,
-    'batch_size': 64,
+    'batch_size': 128,
     'optimizer': 'adam',
     'lr': 2e-4,
     'wd': 0,
     'beta1': 0.5,
-    'epochs': 200,
+    'epochs': 5,
     'latent_size': 64,
     'nz': 100,
     'ndf': 64,
@@ -40,7 +40,8 @@ def main():
 def train(dataset, data):
   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-  sample_dir = 'samples'
+  sample_dir = os.path.join('samples', dataset)
+  weights_dir = os.path.join('weights', dataset)
   os.makedirs(sample_dir, exist_ok=True)
 
   if dataset == 'mnist':
@@ -74,6 +75,9 @@ def train(dataset, data):
 
   D = D.to(device)
   G = G.to(device)
+
+  D.apply(utils.weights_init)
+  G.apply(utils.weights_init)
 
   criterion = nn.BCELoss()
   d_optimizer = utils.get_optim(params, D)
@@ -120,24 +124,51 @@ def train(dataset, data):
             .format(epoch, params['epochs'], i + 1, total_step, d_loss.item(), g_loss.item(),
                     real_score.mean().item(), fake_score.mean().item()))  # .item():ゼロ次元Tensorから値を取り出す
 
-    g_losses.append(g_loss.item())
-    d_losses.append(d_loss.item())
+      g_losses.append(g_loss.item())
+      d_losses.append(d_loss.item())
+
     save_image(utils.denorm(images), os.path.join(
         sample_dir, 'real_images.png'))
     save_image(utils.denorm(fake_images), os.path.join(
         sample_dir, 'fake_images-{}.png'.format(epoch + 1)))
 
-  torch.save(G.state_dict(), 'G.ckpt')
-  torch.save(D.state_dict(), 'D.ckpt')
+  torch.save(G.state_dict(), os.path.join(weights_dir, 'G.ckpt'))
+  torch.save(D.state_dict(), os.path.join(weights_dir, 'D.ckpt'))
 
   plt.figure(figsize=(10, 5))
   plt.title("Generator and Discriminator Loss During Training")
-  plt.plot(g_losses, label="G")
-  plt.plot(d_losses, label="D")
+  plt.plot(g_losses, label="Generator")
+  plt.plot(d_losses, label="Discriminator")
   plt.xlabel("iterations")
   plt.ylabel("Loss")
   plt.legend()
-  plt.show()
+  plt.savefig(os.path.join(sample_dir, 'loss.png'))
+
+
+@main.command()
+@click.option('--dataset', type=str, default='mnist')
+def generate(dataset):
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+  sample_dir = os.path.join('samples', dataset)
+  weights_dir = os.path.join('weights', dataset)
+  os.makedirs(sample_dir, exist_ok=True)
+
+  G = models.Generator(params['nz'], params['ngf'], params['nc'])
+  G.load_state_dict(torch.load(os.path.join(weights_dir, 'G.ckpt')))
+  G.eval()
+  G = G.to(device)
+
+  with torch.no_grad():
+    z = torch.randn(params['batch_size'], params['nz'], 1, 1).to(device)
+    fake_images = G(z)
+
+  dt_now = datetime.datetime.now()
+  now_str = dt_now.strftime('%y%m%d%H%M%S')
+  save_image(utils.denorm(fake_images), os.path.join(
+      sample_dir, 'fake_images_{}.png'.format(now_str)))
+  print('Saved Image ' + os.path.join(sample_dir,
+                                      'fake_images_{}.png'.format(now_str)))
 
 
 if __name__ == '__main__':
